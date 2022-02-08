32f3fba6c63e55718bec965437854a4fd630df85
kunduz11 <usenbaeva.kunduz@gmail.com>
2022-02-08 10:06:55 +0600

    change lesson1.py and add func

--- lesson1.py/lesson1.py
+++ lesson1.py/lesson1.py
@@ -1,7 +1,11 @@
 # map(), filter(), reduce()
 
-def add(x):
-    return x + 7
-lists = [3, 5, 8,12]
-add_to = list(map(add, lists))
-print(add_to)
+# def add(x):
+#     return x + 7
+# lists = [3, 5, 8,12]
+# add_to = list(map(add, lists))
+# print(add_to)
+
+
+def add(x, y):
+    return x + y
